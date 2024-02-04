e83f311a1e1e88d6de3c5341622d1bf8d26799b3
parth <parth@foundationalai.io>
2024-02-04 04:50:06 -0800

    Output parser and Agent updtate

--- backend/app/data_loader.py
+++ backend/app/data_loader.py
@@ -10,13 +10,14 @@ from llama_index import (
     Document,
     StorageContext,
     download_loader,
+    load_index_from_storage,
 )
 import cassio
 from llama_index.vector_stores import CassandraVectorStore
 from llama_index.vector_stores.types import ExactMatchFilter, MetadataFilters
 
-
 load_dotenv()
+
 ASTRA_DB_APPLICATION_TOKEN = os.environ.get("ASTRA_DB_APPLICATION_TOKEN")
 ASTRA_DB_API_ENDPOINT = os.environ.get("ASTRA_DB_API_ENDPOINT")
 ASTRA_DB_ID = os.environ.get("ASTRA_DB_ID")
@@ -29,7 +30,7 @@ cassio.init(
     keyspace=ASTRA_DB_KEYSPACE,
 )
 cassandra_store = CassandraVectorStore(
-    table="All_10_Stock_Earnings", embedding_dimension=1536
+    table="All_Stock_Earnings", embedding_dimension=1536
 )
 
 
@@ -37,30 +38,28 @@ cassandra_store = CassandraVectorStore(
 # load documents
 
 
-tickers = ["Meta"]
-years = ["2021", "2022", "2023"]
-Quarters = ["First", "Second", 'Third', "Fourth"]
+# tickers = ["Meta"]
+# years = ["2021", "2022", "2023"]
+# Quarters = ["First", "Second", 'Third', "Fourth"]
 
-for tick in tickers:
-    for y in years:
-        for q in Quarters:
-            collection_name = f"{tick}_{y}_Q{q}"
+# for tick in tickers:
+#     for y in years:
+#         for q in Quarters:
+#             collection_name = f"{tick}_{y}_Q{q}"
 
-            my_file_metadata = lambda x: {"ticker": tick, "year": y, "quarter": q}
-            md_document = SimpleDirectoryReader(input_files=[f"/Users/parthjain/Desktop/project/ragaroes/pdf_files/{tick}-{q}-Quarter-{y}.pdf"], file_metadata=my_file_metadata).load_data()
-            # Load Nodes into a Vector Store: Insert these nodes into your PostgresVectorStore.
+#             my_file_metadata = lambda x: {"ticker": tick, "year": y, "quarter": q}
+#             md_document = SimpleDirectoryReader(input_files=[f"/Users/parthjain/Desktop/project/ragaroes/pdf_files/{tick}-{q}-Quarter-{y}.pdf"], file_metadata=my_file_metadata).load_data()
+#             md_index = VectorStoreIndex.from_vector_store(vector_store= cassandra_store, document_store=md_document)
 
-            md_storage_context = StorageContext.from_defaults(vector_store=cassandra_store)
 
-            md_index = VectorStoreIndex.from_documents(
-                md_document, storage_context=md_storage_context)
+md_index = VectorStoreIndex.from_vector_store(vector_store= cassandra_store)
 
 md_query_engine = md_index.as_query_engine(
     filters=MetadataFilters(
         filters=[ExactMatchFilter(key="ticker", value="Meta"), ExactMatchFilter(key="year", value="2021"), ExactMatchFilter(key="quarter", value="First")]
     )
 )
-response = md_query_engine.query("Earnings of Meta in 2021 first quarter")
+response = md_query_engine.query("Earnings of Meta in this quarter")
 print(response.response)
 
 md_query_engine = md_index.as_query_engine(
@@ -68,12 +67,21 @@ md_query_engine = md_index.as_query_engine(
         filters=[ExactMatchFilter(key="ticker", value="Meta"), ExactMatchFilter(key="year", value="2022"), ExactMatchFilter(key="quarter", value="First")]
     )
 )
-response = md_query_engine.query("Earnings of Meta in 2022 First quarter")
+response = md_query_engine.query("Earnings of Meta in this quarter")
 print(response.response)
 
 
+md_query_engine = md_index.as_query_engine(
+    filters=MetadataFilters(
+        filters=[ExactMatchFilter(key="ticker", value="Meta"), ExactMatchFilter(key="year", value="2023"), ExactMatchFilter(key="quarter", value="Fourth")]
+    )
+)
+response = md_query_engine.query("Earnings of Meta in this quarter")
+print(response.response)
+
 
         
+
 
 
 
